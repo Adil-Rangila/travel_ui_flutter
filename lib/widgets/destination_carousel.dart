8dff2cad5d9eff9ab1782a9e6a280a0f1d8cf2b9
Adil Ahmed <adil.rangila@gmail.com>
2020-06-16 01:16:40 +0500

    Destination Carousel title UI 50% completed

--- lib/widgets/destination_carousel.dart
+++ lib/widgets/destination_carousel.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:traveluiflutter/models/destination_model.dart';
 
 //destination carousel ............. UI
 
@@ -33,6 +35,58 @@ class DestinationCarousel extends StatelessWidget {
                 ),
               ),
             ],
+          ),
+        ),
+        //horizontal stake destination ui......................
+        Container(
+          height: 300.0,
+          color: Colors.blue,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: destinations.length,
+            itemBuilder: (BuildContext context, int index) {
+              Destination destination = destinations[index];
+              return Container(
+                margin: EdgeInsets.all(8.0),
+                width: 200.0,
+                color: Colors.red,
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      height: 120.0,
+                      width: 200.0,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              '${destination.activities.length} Activites',
+                              style: TextStyle(
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 1.2,
+                              ),
+                            ),
+                            Text(
+                              destination.description,
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
         ),
       ],
