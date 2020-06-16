aaefe81fc72c5e89ffa9a38aeccd811875d3010f
Adil Ahmed <adil.rangila@gmail.com>
2020-06-16 15:32:57 +0500

    Destination Carosule 100% completed with image with stake text and style.

--- lib/widgets/destination_carousel.dart
+++ lib/widgets/destination_carousel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:traveluiflutter/models/destination_model.dart';
 
 //destination carousel ............. UI
@@ -40,7 +41,6 @@ class DestinationCarousel extends StatelessWidget {
         //horizontal stake destination ui......................
         Container(
           height: 300.0,
-          color: Colors.blue,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
             itemCount: destinations.length,
@@ -49,38 +49,104 @@ class DestinationCarousel extends StatelessWidget {
               return Container(
                 margin: EdgeInsets.all(8.0),
                 width: 200.0,
-                color: Colors.red,
                 child: Stack(
+                  alignment: Alignment.topCenter,
                   children: <Widget>[
-                    Container(
-                      height: 120.0,
-                      width: 200.0,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              '${destination.activities.length} Activites',
-                              style: TextStyle(
-                                fontSize: 22.0,
-                                fontWeight: FontWeight.w600,
-                                letterSpacing: 1.2,
-                              ),
-                            ),
-                            Text(
-                              destination.description,
-                              style: TextStyle(
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ],
+                    //title and description ui........................
+                    Positioned(
+                      bottom: 15.0,
+                      child: Container(
+                        height: 120.0,
+                        width: 200.0,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                '${destination.activities.length} Activites',
+                                style: TextStyle(
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.w600,
+                                  letterSpacing: 1.2,
+                                ),
+                              ),
+                              Text(
+                                destination.description,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    //image stake..........................ui
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(20.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26,
+                            offset: Offset(0.0, 2.0),
+                            blurRadius: 6.0,
+                          ),
+                        ],
+                      ),
+                      //imge and tex stake ....................ui
+                      child: Stack(
+                        children: <Widget>[
+                          ClipRRect(
+                            child: Image(
+                              image: AssetImage(destination.imageUrl),
+                              fit: BoxFit.cover,
+                              height: 180.0,
+                              width: 180.0,
+                            ),
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          Positioned(
+                            left:10.0,
+                            bottom:10.0,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  destination.city,
+                                  style: TextStyle(
+                                    fontSize: 24.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600,
+                                    letterSpacing: 1.2,
+                                  ),
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    Icon(
+                                      FontAwesomeIcons.locationArrow,
+                                      size: 10.0,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(width: 5.0),
+                                    Text(
+                                      destination.country,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
